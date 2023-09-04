06f452e4564558f41f0e2e689585f2fdcd5a79f2
Tyler Latshaw <tylerlatshaw@gmail.com>
2023-09-04 18:15:45 -0400

    Refactored layouts to accomodate gym page

--- src/app/(main-site)/resume/page.tsx
+++ src/app/(main-site)/resume/page.tsx
@@ -5,5 +5,13 @@ export const metadata: Metadata = {
 }
 
 export default function Page() {
-  return <h1>Resume</h1>
+  return <>
+    <div className="h-full mb-16 md:pb-8 md:mb-28">
+      <div className="container pt-24 md:pt-36 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+
+        <h1>Coming Soon</h1>
+
+      </div>
+    </div>
+  </>
 }
