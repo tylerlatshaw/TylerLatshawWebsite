e4aa66660e6cb23b12771d12ee638fa6dbf6c648
Tyler Latshaw <tylerlatshaw@gmail.com>
2023-09-04 15:56:42 -0400

    Added Privacy Policy

--- src/app/(main-site)/privacy-policy/page.tsx
+++ src/app/(main-site)/privacy-policy/page.tsx
@@ -1,9 +1,16 @@
+import PrivacyPolicy from '@/components/privacy-policy'
 import { Metadata } from 'next'
- 
+
 export const metadata: Metadata = {
   title: 'Privacy Policy',
 }
 
 export default function Page() {
-  return <h1>Privacy Policy</h1>
+  return <>
+    <div className="h-full mb-16 md:pb-8 md:mb-28">
+      <div className="container pt-24 md:pt-36 mx-auto block text-md flex">
+        <PrivacyPolicy />
+      </div>
+    </div>
+  </>
 }
