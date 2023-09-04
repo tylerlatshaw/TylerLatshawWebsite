19a96ccc3b7989527a3e75052d9a6116714f5d59
Tyler Latshaw <tylerlatshaw@gmail.com>
2023-09-04 16:17:09 -0400

    Added privacy policy

--- src/app/(main-site)/privacy-policy/page.tsx
+++ src/app/(main-site)/privacy-policy/page.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 export default function Page() {
   return <>
     <div className="h-full mb-16 md:pb-8 md:mb-28">
-      <div className="container pt-24 md:pt-36 mx-auto block text-md flex">
+      <div className="container flex pt-24 md:pt-36 mx-auto block text-md">
         <PrivacyPolicy />
       </div>
     </div>
