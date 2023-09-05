4f9f547ee15005937d10229bfbacc2df3ab6b867
Tyler Latshaw <tylerlatshaw@gmail.com>
2023-09-05 17:13:14 -0400

    Test commit

--- src/app/(main-site)/sitemap/page.tsx
+++ src/app/(main-site)/sitemap/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
     <div className="h-full mb-16 md:pb-8 md:mb-28">
       <div className="container pt-24 md:pt-36 mx-auto flex flex-wrap flex-col md:flex-row items-center">
 
-        <h1>Coming Soon</h1>
+        <h1>This is my sitemap</h1>
 
       </div>
     </div>
