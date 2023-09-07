bb5351dc52d555cee8b14c8472addb58f16d1763
Tyler Latshaw <tylerlatshaw@gmail.com>
2023-09-07 19:46:37 -0400

    test email

--- src/app/email-test/page.tsx
+++ src/app/email-test/page.tsx
@@ -1,11 +1,13 @@
 import { Resend } from 'resend';
 import EmailTemplate from '@/components/emails/sample-vercel'
+
 const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
+const fromAddress = process.env.NEXT_PUBLIC_RESEND_FROM;
 
 async function TestEmail () {
   try {
     const data = await resend.emails.send({
-      from: 'Acme <onboarding@resend.dev>',
+      from: fromAddress,
       to: ['tylerlatshaw@gmail.com'],
       subject: 'Hello World',
       react: <EmailTemplate />
