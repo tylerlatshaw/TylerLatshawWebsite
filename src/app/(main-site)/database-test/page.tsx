b1d158f3bbbfc408192aff4449756987dbcf9341
Tyler Latshaw <tylerlatshaw@gmail.com>
2023-09-11 19:53:44 -0400

    commented out db-test for build error

--- src/app/(main-site)/database-test/page.tsx
+++ src/app/(main-site)/database-test/page.tsx
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client'
+//import { PrismaClient } from '@prisma/client'
 
 import { Metadata } from 'next'
 
@@ -6,20 +6,20 @@ export const metadata: Metadata = {
     title: 'Database Test',
 }
 
-const prisma = new PrismaClient()
+//const prisma = new PrismaClient()
 
 export default async function Page() {
-    const artists = await prisma.artists.findMany();
-    prisma.$disconnect();
+    //const artists = await prisma.artists.findMany();
+    //prisma.$disconnect();
 
     return <>
         <div className="h-full mb-16 md:pb-8 md:mb-28">
             <div className="container pt-24 md:pt-36 mx-auto flex flex-wrap flex-col md:flex-row items-center">
-                {artists.map(a =>
+                {/* {artists.map(a =>
                     <>
                         <span>{a.Name}</span>
                         <br />
-                    </>)}
+                    </>)} */}
             </div>
         </div>
     </>
