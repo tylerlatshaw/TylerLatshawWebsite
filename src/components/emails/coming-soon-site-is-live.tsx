fe7aeca6f044f334e083945a68867800093f32f5
Tyler Latshaw <tylerlatshaw@gmail.com>
2023-09-11 17:11:34 -0400

    Updated email template header to props

--- src/components/emails/coming-soon-site-is-live.tsx
+++ src/components/emails/coming-soon-site-is-live.tsx
@@ -14,7 +14,20 @@ import {
 } from '@react-email/components';
 import * as React from 'react';
 
-export const EmailTemplate = () => (
+interface EmailVariables {
+    messageData: {
+        date: string;
+        dateTime: string;
+        title: string;
+        name: string;
+        email: string;
+        message: string;
+        source: string;
+        referringPage: string;
+    }
+}
+
+export const EmailTemplate = (props: EmailVariables) => (
     <Html>
         <Head />
         <Preview>I cannot wait to hear what you think!</Preview>
@@ -33,7 +46,7 @@ export const EmailTemplate = () => (
                 </Section>
                 <Container style={container}>
                     <Heading style={heading}>
-                        Exciting News: My Website is Now <strong>LIVE</strong>!&nbsp;🎉
+                        {props.messageData.title}
                     </Heading>
 
                     <Section style={mainSection}>
