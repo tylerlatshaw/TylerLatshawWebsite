a31630bbedf109f918ad26b9a276c80aaf775ee2
Tyler Latshaw <tylerlatshaw@gmail.com>
2024-02-16 20:01:58 -0500

    Changed sitemap generation

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,77 @@
+/** @type {import('next-sitemap').IConfig} */
+
 module.exports = {
-    siteUrl: "https://tylerlatshaw.com",
+    siteUrl: process.env.BASE_URL,
+    generateIndexSitemap: false,
+    changefreq: "monthly",
     generateRobotsTxt: true,
+    exclude: ["/api/*", "/developer", "/development", "/sitemap.xml"],
+    additionalPaths: async (config) => {
+        const result = [];
+        
+        result.push({
+          loc: "/portfolio/project-management-system-report",
+          changefreq: "yearly",
+          priority: 0.6,
+          lastmod: new Date().toISOString(),
+        });
+        
+        result.push({
+          loc: "/portfolio/database-management-system-proposal",
+          changefreq: "yearly",
+          priority: 0.6,
+          lastmod: new Date().toISOString(),
+        });
+        
+        result.push({
+          loc: "/portfolio/equifax-incident-report",
+          changefreq: "yearly",
+          priority: 0.6,
+          lastmod: new Date().toISOString(),
+        });
+        
+        result.push({
+          loc: "/portfolio/enterprise-resource-planning",
+          changefreq: "yearly",
+          priority: 0.6,
+          lastmod: new Date().toISOString(),
+        });
+        
+        result.push({
+          loc: "/portfolio/automate-your-day",
+          changefreq: "yearly",
+          priority: 0.6,
+          lastmod: new Date().toISOString(),
+        });
+        
+        result.push({
+          loc: "/portfolio/network-assessment",
+          changefreq: "yearly",
+          priority: 0.6,
+          lastmod: new Date().toISOString(),
+        });
+        
+        result.push({
+          loc: "/portfolio/medical-informatics-project-plan",
+          changefreq: "yearly",
+          priority: 0.6,
+          lastmod: new Date().toISOString(),
+        });
+        
+        result.push({
+          loc: "/portfolio/honors-program-transition",
+          changefreq: "yearly",
+          priority: 0.6,
+          lastmod: new Date().toISOString(),
+        });
+        
+        result.push({
+          loc: "/portfolio/canning-buying-guide",
+          changefreq: "yearly",
+          priority: 0.6,
+          lastmod: new Date().toISOString(),
+        });
+            
+        return result;
+      },    
 };  
