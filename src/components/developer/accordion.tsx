ae7a5ead7d65a326a5d8d4eadb3d2fd05334d3d2
Tyler Latshaw <tylerlatshaw@gmail.com>
2024-05-03 22:03:45 -0400

    Updated database details

--- src/components/developer/accordion.tsx
+++ src/components/developer/accordion.tsx
@@ -15,7 +15,7 @@ export default function Accordion({ items }: { items: any; }): JSX.Element {
         <div className="developer-accordion px-3 sm:px-0 mx-auto w-full md:w-1/2 space-y-4">
             {items.map((item: DevItems, index: number) => (
                 index === activeIndex ? <>
-                    <div key={item.title} className="min-w-full bg-gray-700 border-2 border-green-500 rounded-lg">
+                    <div key={item.title + activeIndex} className="min-w-full bg-gray-700 border-2 border-green-500 rounded-lg">
                         <button onClick={() => handleClick(index)} className="w-full p-3 text-left text-2xl font-semibold text-green-500">{item.title}</button>
                         {index === activeIndex && <>
                             <div className="p-3">
@@ -27,7 +27,7 @@ export default function Accordion({ items }: { items: any; }): JSX.Element {
                         }
                     </div>
                 </> : <>
-                    <div key={item.title} className="min-w-full bg-gray-500 hover:bg-gray-600 border-2 rounded-lg">
+                    <div key={item.title + activeIndex} className="min-w-full bg-gray-500 hover:bg-gray-600 border-2 rounded-lg">
                         <button onClick={() => handleClick(index)} className="w-full p-3 text-left text-2xl font-semibold text-white">{item.title}</button>
                     </div>
                 </>
