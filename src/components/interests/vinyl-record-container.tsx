774ed10462c0324892a19459f94fcd4621cb64c7
Tyler Latshaw <tylerlatshaw@gmail.com>
2024-05-05 18:38:25 -0400

    Updated API routes and converted interests to SSR

--- src/components/interests/vinyl-record-container.tsx
+++ src/components/interests/vinyl-record-container.tsx
@@ -1,25 +1,36 @@
-"use client";
-
-import { useEffect, useState } from "react";
-import axios from "axios";
-
 import type { RecordDataType } from "@/app/lib/type-library";
 import VinylRecordCard from "./vinyl-record-card";
 
 export default function VinylRecordContainer() {
-    const [records, setRecords] = useState<RecordDataType[]>([]);
 
-    useEffect(() => {
-        axios.get("/api/dev-get-record-data").then((response) => {
-            setRecords(response.data);
-        });
-    }, []);
+    async function getRecordData() {
+        const res = await fetch(process.env.BASE_URL + "/api/get-record-data");
+        const data = await res.json();
+
+        return data;
+    }
+
+    async function generateRecordCard() {
+        const data: RecordDataType[] = await getRecordData();
+
+        return <>
+            {
+                data.map((record) => {
+                    return <>
+                        <div key={record.RecordId} className="flex items-stretch">
+                            {
+                                VinylRecordCard(record)
+                            }
+                        </div>
+                    </>;
+                })
+            }
+        </>;
+    }
 
     return <>
-        {records.map((record) => (
-            <div key={record.RecordId} className="flex items-stretch">
-                {VinylRecordCard(record)}
-            </div>
-        ))}
+        {
+            generateRecordCard()
+        }
     </>;
 }
